6992c71af440d88f449cd6dfa873570fa2ee4a9e
Vivek Mittal <42748450+vivek1909@users.noreply.github.com>
2020-04-27 00:24:01 +0530

    + sinkDown function

--- Binary Heaps/app.js
+++ Binary Heaps/app.js
@@ -24,6 +24,22 @@ class MaxBinaryHeap {
       index = parentIndex;
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+
+    this.sinkDown();
+
+    return max;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+  }
 }
 
 let heap = new MaxBinaryHeap();
